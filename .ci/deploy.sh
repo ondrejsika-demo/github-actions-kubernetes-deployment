4f6c32447136f177b7b5e38872e0dbb71afd9573
Ondrej Sika <ondrej@ondrejsika.com>
2022-03-26 15:27:37 +0100

    WIP7

---
+++ .ci/deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+echo $KUBECONFIG_BASE64 | base64 --decode > .kubeconfig.yml
+export KUBECONFIG=.kubeconfig.yml
+
+helm upgrade --install hello hello-world \
+    --repo https://helm.sikalabs.io \
+    --namespace test-github-actions --create-namespace \
+    --set host=hello.ybug.dev
