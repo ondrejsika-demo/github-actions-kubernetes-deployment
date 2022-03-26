4ac01443cccf73d830a0b0b29fe95f61556f32bb
Ondrej Sika <ondrej@ondrejsika.com>
2022-03-26 15:48:27 +0100

    WIP14

--- .ci/deploy.sh
+++ .ci/deploy.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
-echo $KUBECONFIG_BASE64 | base64 --decode > .kubeconfig.yml
 export KUBECONFIG=.kubeconfig.yml
 
-env
-cat .kubeconfig.yml
 helm upgrade --install hello hello-world \
     --repo https://helm.sikalabs.io \
     --namespace test-github-actions --create-namespace \
